04c7e58d67f2fb9fab04d3e39471570204955cee
Sender Rodrigues dos Santos <sender.rsantos@gmail.com>
2020-04-13 15:02:46 -0300

    EXERCICIO EXEMPLO LAMBDA

---
+++ ExercicioLambda/Entidades/Produto.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace ExercicioLambda.Entidades {
+    class Produto {
+
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+
+        public Produto(string nome, double preco) {
+            Nome = nome;
+            Preco = preco;
+        }
+    }
+}
--- ExercicioLambda/Program.cs
+++ ExercicioLambda/Program.cs
@@ -1,9 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
+using ExercicioLambda.Entidades;
+using System.IO;
+using System.Globalization;
+using System.Linq;
 
 namespace ExercicioLambda {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            Console.Write("Entre com o caminho completo do arquivo: ");
+            string caminho = Console.ReadLine();
+
+            List<Produto> lista = new List<Produto>();
+
+            using (StreamReader sr = File.OpenText(caminho)) {
+                while (!sr.EndOfStream) {
+                    //separando os campos com vetor
+                    string[] camposProduto = sr.ReadLine().Split(',');
+                    //pegando o campo nome
+                    string nome = camposProduto[0];
+                    //pegando o campo preco
+                    double preco = double.Parse(camposProduto[1], CultureInfo.InvariantCulture);
+                    //criando uma lista com o nome e o preco
+                    lista.Add(new Produto(nome, preco));             
+
+                }
+            }
+            //para encontrar a média do produto. transformamos uma lista em uma lista de double
+            //e utilizando o defaultIfEmpty para colocar 0.0 caso retorne vazio.
+            var media = lista.Select(p => p.Preco).DefaultIfEmpty(0.0).Average();
+            Console.WriteLine("Preço Médio = " + media.ToString("F2", CultureInfo.InvariantCulture));
+            //pegar os nomes onde o preço é abaixo do preço médio e em ordem descrecente por nome.
+            var nomes = lista.Where(p => p.Preco < media).OrderByDescending(p => p.Nome).Select(p => p.Nome);
+            //fazendo um foreach para percorrer cada nome.
+            foreach (string nome in nomes) {
+                Console.WriteLine(nome);
+            }
+
+
         }
     }
 }
